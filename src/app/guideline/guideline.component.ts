03b547df756f8b16222a5e6b37cb8af9793266cf
Wigor <wumortal@outlook.com>
2020-06-06 19:17:55 +0800

    [update] 图片地址调整

--- src/app/guideline/guideline.component.ts
+++ src/app/guideline/guideline.component.ts
@@ -8,9 +8,18 @@ import { HttpClient } from '@angular/common/http';
 })
 export class GuidelineComponent implements OnInit {
 
+  guidelineImageSrcList = [
+    't6zdsg.jpg',
+    't6zaQS.jpg',
+    't6zNz8.jpg',
+    't6zDds.jpg',
+    't6zwLQ.jpg',
+    't6zBZj.jpg',
+    't6zron.jpg'
+  ];
   sayingMaxCount = 1111;
-  saying: string;
-  guidelineImageSrc: string;
+  saying = '正在生成...';
+  currentGuidelineImageSrc: string;
 
   constructor(
     private httpClient: HttpClient
@@ -18,7 +27,7 @@ export class GuidelineComponent implements OnInit {
 
   ngOnInit() {
     const week = new Date().getDay();
-    this.guidelineImageSrc = `/assets/guidelineImages/${week}.jpg`;
+    this.currentGuidelineImageSrc = `https://s1.ax1x.com/2020/06/06/${this.guidelineImageSrcList[week]}`;
 
     this.httpClient.get<Array<string>>('/assets/saying.json').subscribe(res => {
       const index = Math.ceil(Math.random() * this.sayingMaxCount);
